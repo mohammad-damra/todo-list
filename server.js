b878af95bed6970b2383dad639c645b934b17af5
mohammad-damra <mohammad.s.damra@gmail.com>
2025-03-17 23:34:50 +0300

    add the server file (mohammad)

---
+++ server.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const authRoutes = require("./src/routes/auth");
+const todoRoutes = require("./src/routes/todos");
+const authMiddleware = require("./src/middleware/auth");
+const path = require("path");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.static("public"));
+
+// Database connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error(err));
+
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api/todos", authMiddleware, todoRoutes);
+
+// Handle wrong paths
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  console.log("http://localhost:5000/login.html");
+});
